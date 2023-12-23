71ede117ebd1bf0a47f1f5a04f250a6628fc0c6b
Nicolas Martinez <kurulimpah@gmail.com>
2023-12-23 17:07:34 +0100

    ajout de la gallery sur le landing

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,21 @@
+import { PropTypes } from "prop-types";
+
+const Card = ({ title, cover}) => {
+    return (
+        <div className="card">
+            <img src={cover} alt={title} className="card-img" />
+            <p className="card-title">{title}</p>
+        </div>
+    )
+};
+
+Card.propTypes = {
+    title: PropTypes.string.isRequired,
+    cover: PropTypes.string.isRequired,
+};
+Card.defaultProps = {
+    title: '',
+    cover: '',
+}
+
+export default Card
